b29ed1083eb8ff7bdbdfd31bbc8d4cc1e44ee653
Jorge <j_esquiv@yahoo.com>
2026-05-13 21:00:33 -0600

    Update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 const path = require('path');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const app = express();
